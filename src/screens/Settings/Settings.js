ab6b0cc5d2dce3d4740e7d94c15119893cf45c6d
davidasix <davidandersonsix@gmail.com>
2022-08-16 22:25:10 -0400

    Prettier formatted Settings

--- src/screens/Settings/Settings.js
+++ src/screens/Settings/Settings.js
@@ -1,23 +1,18 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {
-  SafeAreaView,
   View,
   Text,
-  ActivityIndicator,
   TouchableOpacity,
-  TextInput,
   Linking,
   UIManager,
   LayoutAnimation,
   Platform,
-  ToastAndroid
+  ToastAndroid,
 } from 'react-native';
-import DatePicker from 'react-native-date-picker'
 import MCIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import MIcons from 'react-native-vector-icons/MaterialIcons';
-import axios from 'axios';
 const c = require('../../assets/constants');
-import SlideUpModal from '../../SlideUpModal/'
+import SlideUpModal from '../../SlideUpModal/';
 
 if (Platform.OS === 'android') {
   if (UIManager.setLayoutAnimationEnabledExperimental) {
@@ -30,52 +25,70 @@ class Settings extends Component {
     super(props);
     this.state = {
       starSelected: 0,
-      complaint: ''
+      complaint: '',
     };
   }
 
   onPressChangeAuth = () => {
     this.props.updateAuthType(null);
-    this.props.navigation.navigate('auth')
-  }
+    this.props.navigation.navigate('auth');
+  };
 
   onPressStar(num) {
     LayoutAnimation.configureNext({
       duration: 700,
-      create: { type: 'spring', springDamping: 0.4, property: 'scaleY' },
-      update: { type: 'spring', springDamping: 0.4 },
+      create: {type: 'spring', springDamping: 0.4, property: 'scaleY'},
+      update: {type: 'spring', springDamping: 0.4},
     });
-    LayoutAnimation.configureNext({ duration: 50, delete: { type: 'easeIn', springDamping: 0.4, property: 'scaleY' } });
-    this.setState({ starSelected: num});
+    LayoutAnimation.configureNext({
+      duration: 50,
+      delete: {type: 'easeIn', springDamping: 0.4, property: 'scaleY'},
+    });
+    this.setState({starSelected: num});
     if (num > 3) {
       // Allow a delay before switching apps, so that the user can see and visualize the 5 stars
-      setTimeout(() => Linking.openURL('https://play.google.com/store/apps/details?id=com.dave6.www.midwifeassist'), 400)
+      setTimeout(
+        () =>
+          Linking.openURL(
+            'https://play.google.com/store/apps/details?id=com.dave6.www.midwifeassist',
+          ),
+        400,
+      );
     } else {
       this.rating.onPressButton('up');
     }
   }
 
   onPressSubmitComment = () => {
-    this.rating.onPressButton()
-    setTimeout(() => ToastAndroid.showWithGravityAndOffset(
-      "Thank you for your feeback!",
-      ToastAndroid.LONG,
-      ToastAndroid.BOTTOM,
-      25,
-      50
-    ), 650);
-  }
+    this.rating.onPressButton();
+    setTimeout(
+      () =>
+        ToastAndroid.showWithGravityAndOffset(
+          'Thank you for your feeback!',
+          ToastAndroid.LONG,
+          ToastAndroid.BOTTOM,
+          25,
+          50,
+        ),
+      650,
+    );
+  };
 
   stars() {
-    let { theme } = this.props;
+    let {theme} = this.props;
     let stars = [];
-    for(let i = 0; i < 5; i++) {
+    for (let i = 0; i < 5; i++) {
       stars.push(
         <MIcons
           onPress={() => this.onPressStar(i + 1)}
-          name={this.state.starSelected >= i+1 ? 'star' : 'star-border'}
+          name={this.state.starSelected >= i + 1 ? 'star' : 'star-border'}
           size={45}
-          color={this.state.starSelected >= i+1 ? c.themes[theme].accent : c.themes[theme].text} />
+          color={
+            this.state.starSelected >= i + 1
+              ? c.themes[theme].accent
+              : c.themes[theme].text
+          }
+        />,
       );
     }
     return stars;
@@ -83,10 +96,10 @@ class Settings extends Component {
 
   renderRatingModal() {
     //
-    let { theme } = this.props;
+    let {theme} = this.props;
     return (
       <SlideUpModal
-        ref={(ref) => this.rating = ref}
+        ref={ref => (this.rating = ref)}
         style={{
           justifyContent: 'center',
           alignItems: 'center',
@@ -98,136 +111,218 @@ class Settings extends Component {
           elevation: 4,
           backgroundColor: c.themes[theme].background,
           borderColor: c.themes[theme].border,
-
         }}
         peek={0}>
-        <Text style={{ color: c.themes[theme].text, marginVertical: 10 }}>Tell us what you think!</Text>
+        <Text style={{color: c.themes[theme].text, marginVertical: 10}}>
+          Tell us what you think!
+        </Text>
 
-          {this.state.starSelected && this.state.starSelected < 4 ? (
-            <>
-              <TouchableOpacity
-                style={[styles.row, { marginVertical: 10, justifyContent: 'center', backgroundColor: c.themes[theme].accent, borderColor: c.themes[theme].border }]}
-                onPress={this.onPressSubmitComment}>
-                <Text style={{ color: c.themes.dark.text }}>Submit Rating</Text>
-              </TouchableOpacity>
-              <TouchableOpacity
-                style={[styles.row, { marginVertical: 10, justifyContent: 'center', backgroundColor: c.themes[theme].accent, borderColor: c.themes[theme].border }]}
-                onPress={() => Linking.openURL('mailto:davidandersonsix@gmail.com?subject=Midwife Assist - Comment')}>
-                <Text style={{ color: c.themes.dark.text }}>Contact Developer</Text>
-              </TouchableOpacity>
-            </>
-          ) : null}
-        <View style={[styles.row, { marginVertical: 10, borderWidth: 0 }]}>
+        {this.state.starSelected && this.state.starSelected < 4 ? (
+          <>
+            <TouchableOpacity
+              style={[
+                styles.row,
+                {
+                  marginVertical: 10,
+                  justifyContent: 'center',
+                  backgroundColor: c.themes[theme].accent,
+                  borderColor: c.themes[theme].border,
+                },
+              ]}
+              onPress={this.onPressSubmitComment}>
+              <Text style={{color: c.themes.dark.text}}>Submit Rating</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={[
+                styles.row,
+                {
+                  marginVertical: 10,
+                  justifyContent: 'center',
+                  backgroundColor: c.themes[theme].accent,
+                  borderColor: c.themes[theme].border,
+                },
+              ]}
+              onPress={() =>
+                Linking.openURL(
+                  'mailto:davidandersonsix@gmail.com?subject=Midwife Assist - Comment',
+                )
+              }>
+              <Text style={{color: c.themes.dark.text}}>Contact Developer</Text>
+            </TouchableOpacity>
+          </>
+        ) : null}
+        <View style={[styles.row, {marginVertical: 10, borderWidth: 0}]}>
           {this.stars()}
         </View>
-
       </SlideUpModal>
     );
   }
 
   render() {
-    let { theme } = this.props;
+    let {theme} = this.props;
     const invertTheme = theme === 'light' ? 'dark' : 'light';
     return (
-        <View style={[styles.container, { backgroundColor: c.themes[theme].background }]}>
-          <View style={[styles.header, { backgroundColor: c.themes[theme].accent }]}>
-            <View style={{ flex: 1, width: '100%', justifyContent: 'center' }}>
-              <Text style={[{ color: c.themes[theme].lightText, fontSize: 36, marginLeft: 20 }, c.titleFont]}>
-                Settings
-              </Text>
-            </View>
+      <View
+        style={[
+          styles.container,
+          {backgroundColor: c.themes[theme].background},
+        ]}>
+        <View
+          style={[styles.header, {backgroundColor: c.themes[theme].accent}]}>
+          <View style={{flex: 1, width: '100%', justifyContent: 'center'}}>
+            <Text
+              style={[
+                {
+                  color: c.themes[theme].lightText,
+                  fontSize: 36,
+                  marginLeft: 20,
+                },
+                c.titleFont,
+              ]}>
+              Settings
+            </Text>
           </View>
-
-          <View style={[styles.body, { backgroundColor: c.themes[theme].modal, borderColor: c.themes[theme].border }]}>
-
-            <TouchableOpacity
-              onPress={() => this.props.changeTheme(invertTheme)}
-              style={[styles.row, { backgroundColor: c.themes[theme].background, borderColor: c.themes[theme].border }]}>
-              <Text style={[styles.buttonText, { color: c.themes[theme].text }]}>
-                {invertTheme.charAt(0).toUpperCase() + invertTheme.substr(1)} Theme
-              </Text>
-              <MCIcons
-                name='theme-light-dark'
-                size={30}
-                color={c.themes[theme].text} />
-            </TouchableOpacity>
-
-          </View>
-
-          <View style={[styles.body, { backgroundColor: c.themes[theme].modal, borderColor: c.themes[theme].border }]}>
-
-            {this.props.authType !== 'none' &&
-              <TouchableOpacity
-                onPress={() => this.props.navigation.navigate('auth')}
-                style={[styles.row, { backgroundColor: c.themes[theme].background, borderColor: c.themes[theme].border }]}>
-                <Text style={[styles.buttonText, { color: c.themes[theme].text }]}>
-                  Logout
-                </Text>
-                <MCIcons
-                  name='logout'
-                  size={30}
-                  color={c.themes[theme].text} />
-              </TouchableOpacity>
-            }
-            <TouchableOpacity
-              onPress={this.onPressChangeAuth}
-              style={[styles.row, { backgroundColor: c.themes[theme].background, borderColor: c.themes[theme].border }]}>
-              <Text style={[styles.buttonText, { color: c.themes[theme].text }]}>
-                Change Security Option
-              </Text>
-              <MCIcons
-                name='lock'
-                size={30}
-                color={c.themes[theme].text} />
-            </TouchableOpacity>
-
-          </View>
-
-          <View style={[styles.body, { backgroundColor: c.themes[theme].modal, borderColor: c.themes[theme].border }]}>
-
-            <TouchableOpacity
-              onPress={() => this.rating.onPressButton()}
-              style={[styles.row, { backgroundColor: c.themes[theme].background, borderColor: c.themes[theme].border }]}>
-              <Text style={[styles.buttonText, { color: c.themes[theme].text }]}>
-                Rate this App
-              </Text>
-              <MCIcons
-                name='star-outline'
-                size={30}
-                color={c.themes[theme].text} />
-            </TouchableOpacity>
-
-            <TouchableOpacity
-              onPress={() => Linking.openURL('https://www.dave6.com')}
-              style={[styles.row, { backgroundColor: c.themes[theme].background, borderColor: c.themes[theme].border }]}>
-              <Text style={[styles.buttonText, { color: c.themes[theme].text }]}>
-                Developers Website
-              </Text>
-              <MCIcons
-                name='web'
-                size={30}
-                color={c.themes[theme].text} />
-            </TouchableOpacity>
-
-            <TouchableOpacity
-              onPress={() => Linking.openURL('mailto:davidandersonsix@gmail.com?subject=Regarding Midwife Assist App')}
-              style={[styles.row, { backgroundColor: c.themes[theme].background, borderColor: c.themes[theme].border }]}>
-              <Text style={[styles.buttonText, { color: c.themes[theme].text }]}>
-                Contact Developer
-              </Text>
-              <MCIcons
-                name='email-outline'
-                size={30}
-                color={c.themes[theme].text} />
-            </TouchableOpacity>
-
-          </View>
-          {this.renderRatingModal()}
         </View>
+
+        <View
+          style={[
+            styles.body,
+            {
+              backgroundColor: c.themes[theme].modal,
+              borderColor: c.themes[theme].border,
+            },
+          ]}>
+          <TouchableOpacity
+            onPress={() => this.props.changeTheme(invertTheme)}
+            style={[
+              styles.row,
+              {
+                backgroundColor: c.themes[theme].background,
+                borderColor: c.themes[theme].border,
+              },
+            ]}>
+            <Text style={[styles.buttonText, {color: c.themes[theme].text}]}>
+              {invertTheme.charAt(0).toUpperCase() + invertTheme.substr(1)}{' '}
+              Theme
+            </Text>
+            <MCIcons
+              name="theme-light-dark"
+              size={30}
+              color={c.themes[theme].text}
+            />
+          </TouchableOpacity>
+        </View>
+
+        <View
+          style={[
+            styles.body,
+            {
+              backgroundColor: c.themes[theme].modal,
+              borderColor: c.themes[theme].border,
+            },
+          ]}>
+          {this.props.authType !== 'none' && (
+            <TouchableOpacity
+              onPress={() => this.props.navigation.navigate('auth')}
+              style={[
+                styles.row,
+                {
+                  backgroundColor: c.themes[theme].background,
+                  borderColor: c.themes[theme].border,
+                },
+              ]}>
+              <Text style={[styles.buttonText, {color: c.themes[theme].text}]}>
+                Logout
+              </Text>
+              <MCIcons name="logout" size={30} color={c.themes[theme].text} />
+            </TouchableOpacity>
+          )}
+          <TouchableOpacity
+            onPress={this.onPressChangeAuth}
+            style={[
+              styles.row,
+              {
+                backgroundColor: c.themes[theme].background,
+                borderColor: c.themes[theme].border,
+              },
+            ]}>
+            <Text style={[styles.buttonText, {color: c.themes[theme].text}]}>
+              Change Security Option
+            </Text>
+            <MCIcons name="lock" size={30} color={c.themes[theme].text} />
+          </TouchableOpacity>
+        </View>
+
+        <View
+          style={[
+            styles.body,
+            {
+              backgroundColor: c.themes[theme].modal,
+              borderColor: c.themes[theme].border,
+            },
+          ]}>
+          <TouchableOpacity
+            onPress={() => this.rating.onPressButton()}
+            style={[
+              styles.row,
+              {
+                backgroundColor: c.themes[theme].background,
+                borderColor: c.themes[theme].border,
+              },
+            ]}>
+            <Text style={[styles.buttonText, {color: c.themes[theme].text}]}>
+              Rate this App
+            </Text>
+            <MCIcons
+              name="star-outline"
+              size={30}
+              color={c.themes[theme].text}
+            />
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            onPress={() => Linking.openURL('https://www.dave6.com')}
+            style={[
+              styles.row,
+              {
+                backgroundColor: c.themes[theme].background,
+                borderColor: c.themes[theme].border,
+              },
+            ]}>
+            <Text style={[styles.buttonText, {color: c.themes[theme].text}]}>
+              Developers Website
+            </Text>
+            <MCIcons name="web" size={30} color={c.themes[theme].text} />
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            onPress={() =>
+              Linking.openURL(
+                'mailto:davidandersonsix@gmail.com?subject=Regarding Midwife Assist App',
+              )
+            }
+            style={[
+              styles.row,
+              {
+                backgroundColor: c.themes[theme].background,
+                borderColor: c.themes[theme].border,
+              },
+            ]}>
+            <Text style={[styles.buttonText, {color: c.themes[theme].text}]}>
+              Contact Developer
+            </Text>
+            <MCIcons
+              name="email-outline"
+              size={30}
+              color={c.themes[theme].text}
+            />
+          </TouchableOpacity>
+        </View>
+        {this.renderRatingModal()}
+      </View>
     );
   }
-};
-
+}
 
 const styles = {
   container: {
@@ -241,7 +336,7 @@ const styles = {
     width: '100%',
     justifyContent: 'space-between',
     alignItems: 'center',
-    marginBottom: 5
+    marginBottom: 5,
   },
   sectionTop: {
     height: 10,
@@ -251,7 +346,7 @@ const styles = {
     borderTopRightRadius: 10,
     borderTopWidth: 1,
     borderLeftWidth: 1,
-    borderRightWidth: 1
+    borderRightWidth: 1,
   },
   body: {
     flex: 0,
@@ -264,7 +359,7 @@ const styles = {
     marginVertical: 5,
     padding: 10,
     borderRadius: 10,
-    elevation: 1
+    elevation: 1,
   },
   row: {
     width: '100%',
@@ -275,8 +370,7 @@ const styles = {
     borderWidth: 1,
     borderRadius: 10,
     paddingHorizontal: 10,
-    marginVertical: 2.5
-
+    marginVertical: 2.5,
   },
   button: {
     height: 45,
@@ -284,18 +378,17 @@ const styles = {
     justifyContent: 'center',
     alignItems: 'center',
     borderWidth: 1,
-    borderRadius: 10
+    borderRadius: 10,
   },
   buttonText: {
-    fontSize: 16
+    fontSize: 16,
   },
   textInput: {
     flex: 1,
     borderBottomWidth: 1,
     textAlign: 'left',
-    fontSize: 12
-  }
-}
-
+    fontSize: 12,
+  },
+};
 
 export default Settings;
--- src/screens/Settings/index.js
+++ src/screens/Settings/index.js
@@ -1,12 +1,12 @@
-import { connect } from 'react-redux';
+import {connect} from 'react-redux';
 import Settings from './Settings';
 import * as actions from '../../actions';
 
-function mapStateToProps ({ settings, auth }) {
+function mapStateToProps({settings, auth}) {
   return {
     theme: settings.theme,
-    authType: auth.authType
+    authType: auth.authType,
   };
-};
+}
 
 export default connect(mapStateToProps, actions)(Settings);
