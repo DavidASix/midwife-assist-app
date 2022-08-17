390c1de9476f0cd7890be7646204d59f2c067642
davidasix <davidandersonsix@gmail.com>
2022-08-16 21:48:28 -0400

    Removed extra functions from Calculator, moved info popup to main render function

--- src/screens/Calculator/Calculator.js
+++ src/screens/Calculator/Calculator.js
@@ -4,12 +4,10 @@ import {
   Text,
   TouchableOpacity,
   UIManager,
-  LayoutAnimation,
   Platform,
   TextInput,
   Animated,
   ScrollView,
-  Button,
 } from 'react-native';
 import DatePicker from 'react-native-date-picker';
 import DateTimePicker from '@react-native-community/datetimepicker';
@@ -47,121 +45,6 @@ class Calculator extends Component {
     this.from = ['EDD', 'GA', 'LMP'];
   }
 
-  renderInputRow() {
-    const {from, weeks, days, lmpDate, eddDate, gaRecDate} = this.state;
-    const {theme} = this.props;
-    // Change what is rendered depending on the type of input selected
-    switch (from) {
-      case 'GA': {
-        // If you remove the borderWidth from the text input container view the app crashes when you switch to EDD
-        // Doesn't happen when you switch to LMP.
-        //Crash:
-        // Attempt to invoke virtual method 'android.view.viewPareny.getParent()' on a null object ref
-
-        return (
-          <>
-            <View
-              style={[sty.row, {borderWidth: 1, borderColor: 'transparent'}]}>
-              <TextInput
-                style={[
-                  sty.textInput,
-                  {
-                    borderColor: c.themes[theme].border,
-                    color: c.themes[theme].text,
-                  },
-                ]}
-                onChangeText={text => this.setState({weeks: text})}
-                value={weeks}
-                keyboardType="decimal-pad"
-                maxLength={2}
-                placeholder="10"
-                keyboardAppearance={theme}
-                placeholderTextColor={c.themes[theme].text + 60}
-              />
-              <Text style={{marginHorizontal: 10, color: c.themes[theme].text}}>
-                Weeks
-              </Text>
-              <TextInput
-                style={[
-                  sty.textInput,
-                  {
-                    borderColor: c.themes[theme].border,
-                    color: c.themes[theme].text,
-                  },
-                ]}
-                onChangeText={text => {
-                  if (parseInt(text) > 6) {
-                    this.setState({
-                      weeks: `${(parseInt(weeks) || 0) + 1}`,
-                      days: `${parseInt(text) % 7}`,
-                    });
-                  } else {
-                    this.setState({days: text});
-                  }
-                }}
-                value={days}
-                keyboardType="decimal-pad"
-                maxLength={1}
-                placeholder="4"
-                keyboardAppearance={theme}
-                placeholderTextColor={c.themes[theme].text + 60}
-              />
-              <Text style={{marginHorizontal: 10, color: c.themes[theme].text}}>
-                Days
-              </Text>
-            </View>
-
-            <View style={{flex: 1, justifyContent: 'center'}}>
-              <Text style={{fontSize: 14, color: c.themes[theme].text}}>
-                GA recorded on:
-              </Text>
-            </View>
-
-            <DatePicker
-              mode="date"
-              date={gaRecDate}
-              textColor={c.themes[theme].text}
-              fadeToColor={c.themes[theme].modal}
-              style={{height: 100}}
-              onDateChange={newDate => this.setState({gaRecDate: newDate})}
-            />
-          </>
-        );
-      }
-      case 'LMP': {
-        return (
-          <>
-            <DatePicker
-              mode="date"
-              date={lmpDate}
-              textColor={c.themes[theme].text}
-              fadeToColor={c.themes[theme].modal}
-              style={{height: 100}}
-              onDateChange={newDate => this.setState({lmpDate: newDate})}
-            />
-          </>
-        );
-      }
-      case 'EDD': {
-        return (
-          <>
-            <DatePicker
-              mode="date"
-              date={eddDate}
-              textColor={c.themes[theme].text}
-              fadeToColor={c.themes[theme].modal}
-              style={{height: 100}}
-              onDateChange={newDate => this.setState({eddDate: newDate})}
-            />
-          </>
-        );
-      }
-      default: {
-        return <Text>Select a conversion</Text>;
-      }
-    }
-  }
-
   // LMP = now - GA
   // LMP = EDD-280days
   // EDD = LMP + 280
@@ -171,12 +54,14 @@ class Calculator extends Component {
     let res = {ga: `${0} weeks, ${0} days`, edd: '-', lmp: '-'};
     let date;
     let gams;
-    /*
+    /**/
     // From GA
     // Check that either weeks or days is entered in the input
-    if (!weeks && !days) return new Error('Check calc()');
+    if (!weeks) weeks = '0';
+    if (!days) days = '0';
     // Check if strings include anything other than Digits or blank space
-    if (!weeks.match(/^(\s*|\d+)$/) || !days.match(/^(\s*|\d+)$/)) return new Error('Check calc()');
+    if (!weeks.match(/^(\s*|\d+)$/)) weeks = '';
+    if (!days.match(/^(\s*|\d+)$/)) days = '';
     gams =
       (parseInt(weeks || 0) * 7 + parseInt(days || 0)) * c.t.day +
       (Date.now() - gaRecDate);
@@ -184,10 +69,10 @@ class Calculator extends Component {
       ga: `${Math.floor(gams / c.t.week) || 0} weeks, ${
         Math.floor((gams % c.t.week) / c.t.day) || 0
       } days`,
-      edd: new Date(Date.now() - gams + 280 * c.t.day).toDateString(),
-      lmp: new Date(Date.now() - gams).toDateString(),
+      edd: new Date(Date.now() - gams + 280 * c.t.day),
+      lmp: new Date(Date.now() - gams),
     };
-*/
+
     // From LMP
     date = lmpDate;
     gams = Date.now() - date.getTime();
@@ -213,177 +98,7 @@ class Calculator extends Component {
       edd: new Date(date),
       lmp: new Date(eddDate - 280 * c.t.day),
     };
-
     return res;
-  }
-
-  renderResults() {
-    let {theme} = this.props;
-    const {from} = this.state;
-    const res = this.calc();
-    return (
-      <View
-        style={{
-          flex: 2,
-          width: '100%',
-          justifyContent: 'space-around',
-          alignItems: 'center',
-        }}>
-        <View style={[sty.row]}>
-          <Text
-            style={[
-              {marginHorizontal: 10, color: c.themes[theme].text},
-              c.titleFont,
-            ]}>
-            GA:
-          </Text>
-          <Text
-            style={[
-              sty.textInput,
-              {
-                borderColor: c.themes[theme].border,
-                maxWidth: 200,
-                marginHorizontal: 10,
-                color: c.themes[theme].text,
-              },
-            ]}>
-            {res.ga}
-          </Text>
-        </View>
-
-        {from !== 'LMP' && (
-          <View style={[sty.row]}>
-            <Text
-              style={[
-                {marginHorizontal: 10, color: c.themes[theme].text},
-                c.titleFont,
-              ]}>
-              LMP:
-            </Text>
-            <Text
-              style={[
-                sty.textInput,
-                {
-                  borderColor: c.themes[theme].border,
-                  maxWidth: 200,
-                  marginHorizontal: 10,
-                  color: c.themes[theme].text,
-                },
-              ]}>
-              {res.lmp}
-            </Text>
-          </View>
-        )}
-
-        {from !== 'EDD' && (
-          <View style={[sty.row]}>
-            <Text
-              style={[
-                {marginHorizontal: 10, color: c.themes[theme].text},
-                c.titleFont,
-              ]}>
-              EDD:
-            </Text>
-            <Text
-              style={[
-                sty.textInput,
-                {
-                  borderColor: c.themes[theme].border,
-                  maxWidth: 200,
-                  marginHorizontal: 10,
-                  color: c.themes[theme].text,
-                },
-              ]}>
-              {res.edd}
-            </Text>
-          </View>
-        )}
-      </View>
-    );
-  }
-
-  renderSlidingPanel() {
-    const {theme} = this.props;
-    const textColor = {color: c.themes[theme].text};
-    return (
-      <SlideUpModal
-        ref={ref => (this.calcInfo = ref)}
-        style={{
-          justifyContent: 'flex-start',
-          alignItems: 'center',
-          width: '93%',
-          minHeight: '55%',
-          padding: 15,
-          borderRadius: 10,
-          borderWidth: 0.5,
-          elevation: 4,
-          backgroundColor: c.themes[theme].background,
-          borderColor: c.themes[theme].border,
-        }}
-        peek={0}>
-        <View style={sty.infoRow}>
-          <Text style={[sty.infoRowText, textColor, {flex: 1}, c.titleFont]}>
-            GA:
-          </Text>
-          <Text
-            style={[sty.infoRowText, textColor, {flex: 3, fontSize: 16}]}>
-            Gestational Age
-          </Text>
-        </View>
-        <View style={sty.infoRow}>
-          <Text style={[sty.infoRowText, textColor, {flex: 1}, c.titleFont]}>
-            LMP:
-          </Text>
-          <Text
-            style={[sty.infoRowText, textColor, {flex: 3, fontSize: 16}]}>
-            Last Mensteral Period
-          </Text>
-        </View>
-        <View style={sty.infoRow}>
-          <Text style={[sty.infoRowText, textColor, {flex: 1}, c.titleFont]}>
-            EDD:
-          </Text>
-          <Text
-            style={[sty.infoRowText, textColor, {flex: 3, fontSize: 16}]}>
-            Estimated Delivery Date
-          </Text>
-        </View>
-
-        <View
-          style={{
-            flex: 1,
-            minHeight: 20,
-            width: '80%',
-            justifyContent: 'center',
-            alignItems: 'center',
-          }}>
-          <View
-            style={{
-              height: 1,
-              width: '100%',
-              backgroundColor: c.themes[theme].border,
-            }}
-          />
-        </View>
-
-        <View style={sty.infoRow}>
-          <Text style={[sty.infoRowTextSmall, textColor, {flex: 1}]}>
-            These calculations use the same method as "pregnancy wheels".
-          </Text>
-        </View>
-        <View style={sty.infoRow}>
-          <Text style={[sty.infoRowTextSmall, textColor, {flex: 1}]}>
-            Accuracy of the calculation depends on the accuracy of the inputted
-            information.
-          </Text>
-        </View>
-        <View style={sty.infoRow}>
-          <Text style={[sty.infoRowTextSmall, textColor, {flex: 1}]}>
-            Because of this calculations may be subject to margin of error.
-          </Text>
-        </View>
-      </SlideUpModal>
-    );
   }
 
   onScroll = e => {
@@ -402,16 +117,6 @@ class Calculator extends Component {
   };
 
   onPressFrom(from) {
-    /*
-    LayoutAnimation.configureNext({
-      duration: 700,
-      create: {type: 'spring', springDamping: 0.4, property: 'scaleY'},
-      update: {type: 'spring', springDamping: 0.4},
-    });
-    LayoutAnimation.configureNext({
-      duration: 50,
-      delete: {type: 'easeIn', springDamping: 0.4, property: 'scaleY'},
-    });*/
     this.setState({from});
     let index = this.from.indexOf(from);
     this.pageScroll.scrollTo({x: c.device.width * index, y: 0, animated: true});
@@ -429,11 +134,10 @@ class Calculator extends Component {
   }
 
   render() {
-    let {theme} = this.props;
-    // ts is themeshort
-    const thm = c.themes[theme];
+    const thm = c.themes[this.props.theme];
     const {from} = this.state;
     const {weeks, days, lmpDate, eddDate, gaRecDate} = this.state;
+    const textColor = {color: thm.text};
     let calc = this.calc();
     return (
       <>
@@ -546,15 +250,7 @@ class Calculator extends Component {
                     }
                   />
                 )}
-                <View
-                  style={{
-                    borderColor: thm.border,
-                    borderTopWidth: 1,
-                    width: '80%',
-                    alignSelf: 'center',
-                    marginVertical: 10,
-                  }}
-                />
+                <View style={[sty.spacer, {borderColor: thm.border}]} />
                 {/** Output Verbiage **/}
                 <Text style={sty.outputTitle}>
                   Their babies gestational age is roughly:
@@ -576,7 +272,7 @@ class Calculator extends Component {
             <View style={sty.cardContainer}>
               <View style={[sty.card, {backgroundColor: thm.modal}]}>
                 <Text style={[sty.inputTitle, {color: thm.text}]}>
-                  The babies gestational age is...
+                  The baby's gestational age is...
                 </Text>
                 {/** GA Input Row **/}
                 <View style={sty.row}>
@@ -587,7 +283,7 @@ class Calculator extends Component {
                     keyboardType="decimal-pad"
                     maxLength={2}
                     placeholder="10"
-                    keyboardAppearance={theme}
+                    keyboardAppearance={this.props.theme}
                     placeholderTextColor={thm.text + 60}
                   />
                   <Text style={{marginHorizontal: 10, color: thm.text}}>
@@ -600,7 +296,7 @@ class Calculator extends Component {
                     keyboardType="decimal-pad"
                     maxLength={1}
                     placeholder="4"
-                    keyboardAppearance={theme}
+                    keyboardAppearance={this.props.theme}
                     placeholderTextColor={thm.text + 60}
                   />
                   <Text style={{marginHorizontal: 10, color: thm.text}}>
@@ -608,6 +304,22 @@ class Calculator extends Component {
                   </Text>
                 </View>
                 {/** GA Input Row END **/}
+                <View style={[sty.spacer, {borderColor: thm.border}]} />
+                {/** Output Verbiage **/}
+                <Text style={sty.outputTitle}>
+                  The client's estimated due date is:
+                </Text>
+                <Text style={{alignSelf: 'center'}}>
+                  {calc.ga.edd.toDateString()}
+                </Text>
+
+                <Text style={sty.outputTitle}>
+                  The client's last mensteral period started on
+                </Text>
+                <Text style={{alignSelf: 'center'}}>
+                  {calc.ga.lmp.toDateString()}
+                </Text>
+                {/** Output Verbiage End **/}
               </View>
             </View>
             {/** GA Card Render END **/}
@@ -637,15 +349,7 @@ class Calculator extends Component {
                     }
                   />
                 )}
-                <View
-                  style={{
-                    borderColor: thm.border,
-                    borderTopWidth: 1,
-                    width: '80%',
-                    alignSelf: 'center',
-                    marginVertical: 10,
-                  }}
-                />
+                <View style={[sty.spacer, {borderColor: thm.border}]} />
                 {/** Output Verbiage **/}
                 <Text style={sty.outputTitle}>
                   Their babies gestational age is roughly:
@@ -664,7 +368,83 @@ class Calculator extends Component {
             {/** LMP Card Render END **/}
           </ScrollView>
         </View>
-        {this.renderSlidingPanel()}
+        <SlideUpModal
+          ref={ref => (this.calcInfo = ref)}
+          style={{
+            justifyContent: 'flex-start',
+            alignItems: 'center',
+            width: '93%',
+            minHeight: '55%',
+            padding: 15,
+            borderRadius: 10,
+            borderWidth: 0.5,
+            elevation: 4,
+            backgroundColor: thm.background,
+            borderColor: thm.border,
+          }}
+          peek={0}>
+          <View style={sty.infoRow}>
+            <Text style={[sty.infoRowText, textColor, {flex: 1}, c.titleFont]}>
+              GA:
+            </Text>
+            <Text
+              style={[sty.infoRowText, textColor, {flex: 3, fontSize: 16}]}>
+              Gestational Age
+            </Text>
+          </View>
+          <View style={sty.infoRow}>
+            <Text style={[sty.infoRowText, textColor, {flex: 1}, c.titleFont]}>
+              LMP:
+            </Text>
+            <Text
+              style={[sty.infoRowText, textColor, {flex: 3, fontSize: 16}]}>
+              Last Mensteral Period
+            </Text>
+          </View>
+          <View style={sty.infoRow}>
+            <Text style={[sty.infoRowText, textColor, {flex: 1}, c.titleFont]}>
+              EDD:
+            </Text>
+            <Text
+              style={[sty.infoRowText, textColor, {flex: 3, fontSize: 16}]}>
+              Estimated Delivery Date
+            </Text>
+          </View>
+
+          <View
+            style={{
+              flex: 1,
+              minHeight: 20,
+              width: '80%',
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}>
+            <View
+              style={{
+                height: 1,
+                width: '100%',
+                backgroundColor: thm.border,
+              }}
+            />
+          </View>
+
+          <View style={sty.infoRow}>
+            <Text style={[sty.infoRowTextSmall, textColor, {flex: 1}]}>
+              These calculations use the same method as "pregnancy wheels".
+            </Text>
+          </View>
+          <View style={sty.infoRow}>
+            <Text style={[sty.infoRowTextSmall, textColor, {flex: 1}]}>
+              Accuracy of the calculation depends on the accuracy of the inputted
+              information.
+            </Text>
+          </View>
+          <View style={sty.infoRow}>
+            <Text style={[sty.infoRowTextSmall, textColor, {flex: 1}]}>
+              Because of this calculations may be subject to margin of error.
+            </Text>
+          </View>
+        </SlideUpModal>
       </>
     );
   }
@@ -752,6 +532,12 @@ const sty = {
   outputTitle: {
     fontSize: 16,
     marginVertical: 5,
+  },
+  spacer: {
+    borderTopWidth: 1,
+    width: '80%',
+    alignSelf: 'center',
+    marginVertical: 10,
   },
   row: {
     alignItems: 'center',
